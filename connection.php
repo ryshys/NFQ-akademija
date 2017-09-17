a21a861509d39cd1c4b645fccfd720ab3a595f2f
ryshys <ryshys@gmail.com>
2017-09-17 23:03:56 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ function Connect()
 {
  $dbhost = "localhost";
  $dbuser = "nfq";
- $dbpass = "ibumetin123";
+ $dbpass = "";
  $dbname = "nfq";
  
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
